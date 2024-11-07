7dc44aa4ee80be586044ceaf2260ff0a32988bcc
Shayan Daijavad <shayan@daijavad.com>
2024-11-07 10:27:11 -0800

    attempting epochs

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
 CXX = mpic++
 CXXFLAGS = -std=c++2a -Wall
-BOOST_LIBS = -lboost_graph
+
+BOOST_INC = /opt/homebrew/include
+BOOST_LIB = /opt/homebrew/lib
+
+
+BOOST_LIBS = -L$(BOOST_LIB) -lboost_graph
 
 all: test
 
-test: test.o graph.o random_trees.o
-	$(CXX) $(CXXFLAGS) -o test test.o graph.o random_trees.o $(BOOST_LIBS)
+test: test.o dynamic_programming.o glauber_dynamics.o random_trees.o
+	$(CXX) $(CXXFLAGS) -I$(BOOST_INC) -o test test.o dynamic_programming.o glauber_dynamics.o random_trees.o $(BOOST_LIBS)
 # Object file compilation
 test.o: test.cpp dynamic_programming.h glauber_dynamics.h random_trees.h
-	$(CXX) $(CXXFLAGS) -c test.cpp
+	$(CXX) $(CXXFLAGS) -I$(BOOST_INC) -c test.cpp
 dynamic_programming.o: dynamic_programming.cpp dynamic_programming.h
-	$(CXX) $(CXXFLAGS) -c dynamic_programming.cpp
+	$(CXX) $(CXXFLAGS) -I$(BOOST_INC) -c dynamic_programming.cpp
 glauber_dynamics.o: glauber_dynamics.cpp glauber_dynamics.h
-	$(CXX) $(CXXFLAGS) -c glauber_dynamics.cpp
+	$(CXX) $(CXXFLAGS) -I$(BOOST_INC) -c glauber_dynamics.cpp
 random_trees.o: random_trees.cpp random_trees.h
-	$(CXX) $(CXXFLAGS) -c random_trees.cpp
+	$(CXX) $(CXXFLAGS) -I$(BOOST_INC) -c random_trees.cpp
 
 
 clean:
--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 #include <mpi.h>
 
 
-double lambda = 0.001;
+
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
@@ -17,11 +17,19 @@ int main(int argc, char *argv[]) {
 
     Graph g;
     double alpha = 1;
-    int T = 100000;
+    int T = 100;
     double K = 200;
-    double lambda = 1;
+    double lambda = 0.001;
     std::random_device rd;
 
+    int total_epochs = 5;
+    int epochs_per_process = total_epochs / num_processes;
+    int extra_epochs = total_epochs % num_processes;
+    if (rank < extra_epochs) {
+        epochs_per_process++;
+    }
+
+    double alpha_sum_local = 0.0;
 
 
     std::vector<int> prufer_sequence = create_prufer_sequence(500, 378912830);
@@ -34,50 +42,63 @@ int main(int argc, char *argv[]) {
         std::cout << num_ind_sets(g, 0, lambda) << std::endl;
     }
 
-    for (int i = 0; i < m; i++) {
-        auto edge = edges_list[i];
-        int removed_edge_source = boost::source(edge, g);
-        int removed_edge_target = boost::target(edge, g);
+    for (int epoch = 0; epoch < epochs_per_process; epoch++) {
+        for (int i = 0; i < m; i++) {
+            auto edge = edges_list[i];
+            int removed_edge_source = boost::source(edge, g);
+            int removed_edge_target = boost::target(edge, g);
 
 
-        boost::remove_edge(edge, g);
-        // If this is the last edge (the Mth edge), alpha is 2^n
-        if (i == edges_list.size() - 1 && rank == 0) {
-            alpha *= pow(2, num_vertices(g));
-        }
-        else {
-            int samples_per_process = K / num_processes;
-            int remaining_samples = int(K) % num_processes;
-            double local_invalid_sets = 0;
-
-            for (int j = 0; j < samples_per_process; j++) {
-                std::set<int> sample = glauber_dynamics(g, T, rd(), lambda);
-                if (sample.find(removed_edge_source) != sample.end() && sample.find(removed_edge_target) != sample.end()) {
-                    local_invalid_sets++;
-                }
+            boost::remove_edge(edge, g);
+            // If this is the last edge (the Mth edge), alpha is 2^n
+            if (i == edges_list.size() - 1 && rank == 0) {
+                alpha *= pow(2, num_vertices(g));
             }
+            else {
+                int samples_per_process = K / num_processes;
+                int remaining_samples = int(K) % num_processes;
+                double local_invalid_sets = 0;
 
-            if (rank == 0 && remaining_samples > 0) {
-                for (int j = 0; j < remaining_samples; j++) {
+                for (int j = 0; j < samples_per_process; j++) {
                     std::set<int> sample = glauber_dynamics(g, T, rd(), lambda);
                     if (sample.find(removed_edge_source) != sample.end() && sample.find(removed_edge_target) != sample.end()) {
                         local_invalid_sets++;
                     }
                 }
-            }
 
-            double total_invalid_sets = 0;
-            MPI_Reduce(&local_invalid_sets, &total_invalid_sets, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-            if (rank == 0) {
-                // The ratio that I multiply alpha by is the number of sets I sampled from Gi+1 that were also independent sets in Gi over the total number of sets I sampled.
-                alpha *= (K - total_invalid_sets) / K;
+                if (rank == 0 && remaining_samples > 0) {
+                    for (int j = 0; j < remaining_samples; j++) {
+                        std::set<int> sample = glauber_dynamics(g, T, rd(), lambda);
+                        if (sample.find(removed_edge_source) != sample.end() && sample.find(removed_edge_target) != sample.end()) {
+                            local_invalid_sets++;
+                        }
+                    }
+                }
+
+                double total_invalid_sets = 0;
+                MPI_Reduce(&local_invalid_sets, &total_invalid_sets, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+                if (rank == 0) {
+                    // The ratio that I multiply alpha by is the number of sets I sampled from Gi+1 that were also independent sets in Gi over the total number of sets I sampled.
+                    alpha *= (K - total_invalid_sets) / K;
+                }
             }
+        }
+
+        if (rank == 0) {
+            alpha_sum_local += alpha;
+            std::cout << "Process " << rank << " - Alpha for epoch " << epoch + 1 << ": " << alpha << std::endl;
         }
     }
 
+    double global_alpha_sum = 0;
+    MPI_Reduce(&alpha_sum_local, &global_alpha_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
+
     MPI_Finalize();
     if (rank == 0) {
-        std::cout << alpha << std::endl;
+        double alpha_avg = global_alpha_sum / total_epochs;
+        std::cout << "Average alpha over " << total_epochs << " epochs: " << alpha_avg << std::endl;
     }
     return 0;
 }
