c1b2c3809c78ce45a810b8421628e8795a8f23c1
Shayan Daijavad <shayan@daijavad.com>
2024-12-05 10:44:13 -0800

    added threshold for T

--- test.cpp
+++ test.cpp
@@ -26,10 +26,10 @@ int main(int argc, char *argv[]) {
 
     Graph g;
     double alpha = 1;
-    int T = 10000;
+    int T = 1;
     double K = 200;
     double lambda = 1;
-    int n = 256;
+    int n = 10;
     std::random_device rd;
 
 
@@ -55,59 +55,84 @@ int main(int argc, char *argv[]) {
 //    std::cout << "Samples per process: " << samples_per_process << std::endl;
 
 
-    // We need to choose a specific seed so that all cores (which don't share memory) have access to the same graph.
+
     std::vector<int> prufer_sequence = create_prufer_sequence(n, 12781358);
     g = prufer_sequence_to_tree(prufer_sequence);
     auto edges = boost::edges(g);
     std::vector<Graph::edge_descriptor> edges_list(edges.first, edges.second);
     int m = boost::num_edges(g);
 
+    double actual_independent_sets = 0;
     if (rank == 0) {
+        actual_independent_sets = num_ind_sets(g, 0, lambda);
         std::cout << "Lambda = " << lambda << " n = " << n << " K = " << K << " T = " << T << std::endl;
-        std::cout << "Actual Number of Independent Sets: " << num_ind_sets(g, 0, lambda) << std::endl;
+        std::cout << "Actual Number of Independent Sets: " << actual_independent_sets << std::endl;
     }
+    MPI_Bcast(&actual_independent_sets, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    double sum_alpha = 0;
+    bool within_threshold = false;
 
-    for (int i = 0; i < m; i++) {
-        auto edge = edges_list[i];
-        int removed_edge_source = boost::source(edge, g);
-        int removed_edge_target = boost::target(edge, g);
+    while (!within_threshold) {
+        // We need to choose a specific seed so that all cores (which don't share memory) have access to the same graph.
+        std::vector<int> prufer_sequence = create_prufer_sequence(n, 12781358);
+        g = prufer_sequence_to_tree(prufer_sequence);
+        auto edges = boost::edges(g);
+        std::vector<Graph::edge_descriptor> edges_list(edges.first, edges.second);
+        int m = boost::num_edges(g);
+
+        double sum_alpha = 0;
+        alpha = 1;
+        for (int i = 0; i < m; i++) {
+            auto edge = edges_list[i];
+            int removed_edge_source = boost::source(edge, g);
+            int removed_edge_target = boost::target(edge, g);
 
 
-        boost::remove_edge(edge, g);
-        // If this is the last edge (the Mth edge), alpha is 2^n
-        if (i == edges_list.size() - 1 && sub_rank == 0) {
-            alpha *= pow(2, num_vertices(g));
-        }
-        else {
-
-            double local_invalid_sets = 0;
-            for (int s = 0; s < samples_per_process; s++) {
-                std::set<int> sample = glauber_dynamics(g, T, rd(), lambda);
-                if (sample.find(removed_edge_source) != sample.end() && sample.find(removed_edge_target) != sample.end()) {
-                    local_invalid_sets++;
-                }
-            }
-
-            double total_invalid_sets = 0;
-            MPI_Reduce(&local_invalid_sets, &total_invalid_sets, 1, MPI_DOUBLE, MPI_SUM, 0, sub_comm);
-
-            if (sub_rank == 0) {
-                // The ratio that I multiply alpha by is the number of sets I sampled from Gi+1 that were also independent sets in Gi over the total number of sets I sampled.
-                alpha *= (K - total_invalid_sets) / K;
+            boost::remove_edge(edge, g);
+            // If this is the last edge (the Mth edge), alpha is 2^n
+            if (i == edges_list.size() - 1 && sub_rank == 0) {
+                alpha *= pow(2, num_vertices(g));
             }
             else {
-                alpha = 0;
+
+                double local_invalid_sets = 0;
+                for (int s = 0; s < samples_per_process; s++) {
+                    std::set<int> sample = glauber_dynamics(g, T, rd(), lambda);
+                    if (sample.find(removed_edge_source) != sample.end() && sample.find(removed_edge_target) != sample.end()) {
+                        local_invalid_sets++;
+                    }
+                }
+
+                double total_invalid_sets = 0;
+                MPI_Reduce(&local_invalid_sets, &total_invalid_sets, 1, MPI_DOUBLE, MPI_SUM, 0, sub_comm);
+
+                if (sub_rank == 0) {
+                    // The ratio that I multiply alpha by is the number of sets I sampled from Gi+1 that were also independent sets in Gi over the total number of sets I sampled.
+                    alpha *= (K - total_invalid_sets) / K;
+                }
+                else {
+                    alpha = 0;
+                }
             }
         }
-    }
 
-//    std::cout << "World Rank: " << rank << " Sub Rank: " << sub_rank << " Alpha: " << alpha << std::endl;
-    MPI_Reduce(&alpha, &sum_alpha, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
-        std::cout << "Averaged Alpha: " << sum_alpha / (num_processes < epochs ? num_processes : epochs) << std::endl;
+    //    std::cout << "World Rank: " << rank << " Sub Rank: " << sub_rank << " Alpha: " << alpha << std::endl;
+        MPI_Reduce(&alpha, &sum_alpha, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            double averaged_alpha = sum_alpha / (num_processes < epochs ? num_processes : epochs);
+            std::cout << "T: " << T << std::endl;
+            std::cout << "Averaged Alpha: " << averaged_alpha << std::endl;
+
+            if (averaged_alpha <= actual_independent_sets * 1.5) {
+                within_threshold = true;
+            }
+        }
+
+        MPI_Bcast(&within_threshold, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+        T *= 2;
+
     }
 
     MPI_Comm_free(&sub_comm);
